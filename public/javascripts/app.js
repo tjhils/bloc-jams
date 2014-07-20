9110a7e595f56e46ce831f82383658a0e85d44bc
Thomas Hils <thomas@hils.us>
2014-07-20 15:02:17 +0000

    Added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -221,8 +221,9 @@ if (document.URL.match(/\/album.html/)) {
 
 ;require.register("scripts/app", function(exports, require, module) {
   require("./landing");
-  require('./collection');
-  require('./album');
+  require("./collection");
+  require("./album");
+  require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -314,6 +315,28 @@ if (document.URL.match(/\/album.html/)) {
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
     
   });
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+ var tabsContainer = ".user-profile-tabs-container"
+ var selectTabHandler = function(event) {
+   $tab = $(this);
+   $(tabsContainer + " li").removeClass('active');
+   $tab.parent().addClass('active');
+   selectedTabName = $tab.attr('href');
+   console.log(selectedTabName);
+   $(".tab-pane").addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+ };
+ 
+ if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 });
 
 ;
